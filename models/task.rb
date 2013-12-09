e2c5e7c39d0727aa841f8f895451e48640c81737
Nick Stewart <nicholas4@gmail.com>
2013-12-09 03:49:55 +0000

    Add order value to Tasks

--- models/task.rb
+++ models/task.rb
@@ -6,19 +6,29 @@ class Task < Sequel::Model
     ::DB.create_table? ::Task::TABLE_NAME do
       Integer :id, :primary_key => true
       String  :name
-      Boolean :completed, :default => false
+      Boolean :completed,   :default => false
+      Integer :order_value, :null => false
     end
 
-    # Add some data
-    table_handle = ::DB[::Task::TABLE_NAME]
-    table_handle.insert(:name => 'Eat')
-    table_handle.insert(:name => 'Sleep')
-    table_handle.insert(:name => 'Code')
-
     ::Task.dataset = ::Task.dataset
+
+    # Add some data
+    ::Task.create(:name => 'Eat')
+    ::Task.create(:name => 'Sleep')
+    ::Task.create(:name => 'Code')
   end
 
   def self.table_exists?
     ::DB.table_exists?(::Task::TABLE_NAME)
+  end
+
+  def before_save
+    if ::Task.count == 0
+      self.order_value = 1
+    else
+      self.order_value ||= DB[::Task::TABLE_NAME].max(:order_value) + 1
+    end
+
+    super
   end
 end
