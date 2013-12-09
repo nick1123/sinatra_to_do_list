d69b1cb9b0a5184ca345228f1be25226e4cb5dc7
Nick Stewart <nicholas4@gmail.com>
2013-12-09 01:57:15 +0000

    Add List index view

--- driver.rb
+++ driver.rb
@@ -1,6 +1,8 @@
 require "rubygems"
 require "sequel"
 require "sqlite3"
+require "sinatra"
+require "slim"
 
 # Connect to an in-memory database
 DB = Sequel.sqlite
@@ -8,5 +10,11 @@ DB = Sequel.sqlite
 # Require models
 Dir[File.dirname(__FILE__) + '/models/*.rb'].each {|file| require file}
 
+# Create tables and insert dummy data
 ::List.create_table unless ::List.table_exists?
 ::Item.create_table unless ::Item.table_exists?
+
+get '/' do
+  @lists = ::List.all
+  slim :lists
+end
