e7a82be0fd709f3cb58999038e72560d1e4c4fec
Nick Stewart <nicholas4@gmail.com>
2013-12-09 04:39:23 +0000

    Task ordering now works

--- driver.rb
+++ driver.rb
@@ -14,7 +14,13 @@ Dir[File.dirname(__FILE__) + '/models/*.rb'].each {|file| require file}
 ::Task.create_table unless ::Task.table_exists?
 
 get '/' do
-  @incomplete_tasks = ::Task.where(:completed => false)
+  # Switch from Sequel::SQLite::Dataset to Array so we can do operations like:
+  #   @incomplete_tasks[index-1]
+  @incomplete_tasks = []
+  ::Task.where(:completed => false).order(:order_value).each do |task|
+    @incomplete_tasks << task
+  end
+
   @completed_tasks  = ::Task.where(:completed => true)
   slim :tasks
 end
@@ -33,5 +39,16 @@ put '/task/:id' do
   task = ::Task.where(:id => params[:id]).first
   task.completed = !task.completed
   task.save
+  redirect to('/')
+end
+
+put '/swap_order_values/:id1/:id2' do
+  task1 = ::Task.where(:id => params[:id1]).first
+  task2 = ::Task.where(:id => params[:id2]).first
+  tmp = task1.order_value
+  task1.order_value = task2.order_value
+  task2.order_value = tmp
+  task1.save
+  task2.save
   redirect to('/')
 end
